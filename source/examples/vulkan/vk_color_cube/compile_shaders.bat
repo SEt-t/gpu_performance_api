c5332c292407c1db23b49908a2fc6ca5f226d960
Skakov Pavel <pavelsx@gmail.com>
2024-05-26 15:37:33 +0300

    Fix building of VK example

--- source/examples/vulkan/vk_color_cube/compile_shaders.bat
+++ source/examples/vulkan/vk_color_cube/compile_shaders.bat
@@ -5,7 +5,7 @@ SET outDir="%~1"
 
 IF outDir=="" THEN SET outDir=".\"
 
-%VULKAN_SDK%\Bin\glslangValidator.exe -V vk_color_cube_shader.vert -o %outDir%vk_color_cube_shader.vert.spv
-%VULKAN_SDK%\Bin\glslangValidator.exe -V vk_color_cube_shader.frag -o %outDir%vk_color_cube_shader.frag.spv
-%VULKAN_SDK%\Bin\glslangValidator.exe -V vk_color_cube_wireframe_shader.frag -o %outDir%vk_color_cube_wireframe_shader.frag.spv
+"%VULKAN_SDK%\Bin\glslangValidator.exe" -V vk_color_cube_shader.vert -o %outDir%vk_color_cube_shader.vert.spv
+"%VULKAN_SDK%\Bin\glslangValidator.exe" -V vk_color_cube_shader.frag -o %outDir%vk_color_cube_shader.frag.spv
+"%VULKAN_SDK%\Bin\glslangValidator.exe" -V vk_color_cube_wireframe_shader.frag -o %outDir%vk_color_cube_wireframe_shader.frag.spv
 
